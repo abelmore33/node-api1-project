74c34cb2bb35b267e360c87558fa0b3239145a20
Aaron Belmore <belmoreaaron9@gmail.com>
2023-07-28 11:06:15 -0400

    get req is working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,24 @@
 // BUILD YOUR SERVER HERE
+// import server
+const express = require("express");
+const User = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const api = "/api/users";
+
+// instance of express
+const server = express();
+// global middleware
+server.use(express.json());
+
+// endpoints
+
+server.get(`/api/users`, async (req, res) => {
+  try {
+    const user = User.find();
+    console.log(user);
+  } catch (err) {
+    res.status(500).json({ message: `Bad Request ${err.message}` });
+  }
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
